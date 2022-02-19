ad04a256b086843f262bb60bf8d1698e0c5585f8
mahadihasan2 <mahadihasan125643@gmail.com>
2022-02-19 17:15:21 +0600

    spareate js

---
+++ sperate.js
@@ -0,0 +1,7 @@
+const number = [20, 60, 43, 89, 105, 44]
+// console.log(number)
+// console.log(...number)
+const maxNumber = Math.max(67, 45, 43, 32, 22, 450, 780)
+const mathInArray = Math.max(...number)
+console.log(mathInArray)
+console.log(maxNumber)
